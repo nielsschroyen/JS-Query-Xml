a6ec2b06558d00562182ee24ec91faba04626c12
Niels Schroyen <nielsschroye@gmail.com>
2015-12-16 21:41:12 +0100

    use ace as editor

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+
+
 var queryButton = document.getElementById("queryButton");
 var resultDiv = document.getElementById("resultDiv");
-var commandTextBox = document.getElementById("commandTextBox");
-var xmlTextBox = document.getElementById("xmlTextBox");
 var examplesSelectbox = document.getElementById("examplesSelectbox");
 queryButton.addEventListener('click', runQuery, false)
 examplesSelectbox.addEventListener('change', updateExample, false)
+  
+var xmlEditor = ace.edit("xmlEditor");
+xmlEditor.getSession().setMode("ace/mode/xml");
+
+var javascriptEditor = ace.edit("javascriptEditor");
+javascriptEditor.getSession().setMode("ace/mode/javascript");
+
 
 function runQuery() {
     try {
-        var xml = xmlToJSON.parseString(xmlTextBox.value);
-    	resultDiv.innerHTML = html(eval(commandTextBox.value)); 
+        var xml = xmlToJSON.parseString(xmlEditor.getValue());
+    	resultDiv.innerHTML = html(eval(javascriptEditor.getValue())); 
     }
     catch(err) {
         resultDiv.innerHTML = err.message
@@ -21,8 +28,14 @@ function updateExample() {
    var example = xmlExamples[exampleKey]
    
    if(example){
-    xmlTextBox.value = example.xml;
-    commandTextBox.value = example.query;
-    runQuery();
+    //  xmlEditor.selectAll();
+    //  xmlEditor.clearSelection();
+      xmlEditor.setValue(example.xml);
+
+    //  javascriptEditor.selectAll();
+    //  javascriptEditor.clearSelection();
+      javascriptEditor.setValue(example.query);
+
+      runQuery();
    }
 }
