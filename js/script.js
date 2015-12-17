7706d904293a601f87d558bd9941cbbe66eda1e1
Niels Schroyen <nielsschroye@gmail.com>
2015-12-17 21:42:20 +0100

    Bootstrap treeview as resultpane

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-
-
 var queryButton = document.getElementById("queryButton");
-var resultDiv = document.getElementById("resultDiv");
 var examplesSelectbox = document.getElementById("examplesSelectbox");
 queryButton.addEventListener('click', runQuery, false)
 examplesSelectbox.addEventListener('change', updateExample, false)
@@ -12,14 +9,19 @@ xmlEditor.getSession().setMode("ace/mode/xml");
 var javascriptEditor = ace.edit("javascriptEditor");
 javascriptEditor.getSession().setMode("ace/mode/javascript");
 
-
 function runQuery() {
     try {
         var xml = xmlToJSON.parseString(xmlEditor.getValue());
-    	resultDiv.innerHTML = html(eval(javascriptEditor.getValue())); 
+        var result = eval(javascriptEditor.getValue());
+       $('#tree').treeview( {
+            data: objectToTreeview(result),
+            showBorder: false,
+            expandIcon: 'glyphicon glyphicon-chevron-right',
+            collapseIcon: 'glyphicon glyphicon-chevron-down'
+        } );
     }
     catch(err) {
-        resultDiv.innerHTML = err.message
+        alert(err.message);
     }
 }
 
@@ -32,6 +34,42 @@ function updateExample() {
       javascriptEditor.setValue(example.query,-1);
       runQuery();
    }
+}
+
+function objectToTreeview(obj){
+    if(!isObject(obj) && !isArray(obj)){
+        return [{text: obj, selectable: false}];
+    }
+    var arr = [];
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) { 
+        var val = obj[prop];
+
+        if(isObject(val)){
+            arr.push({text: prop, selectable: false, nodes: objectToTreeview(val)});
+        }
+        else {
+            if(isArray(obj)){
+                val.forEach(function(item,index){arr.push({text:prop+ ' ' + (index+1), selectable: false, nodes: objectToTreeview(item)});});
+            }
+                else {                    
+                    arr.push({text: prop +': ' + val, selectable: false });        
+                }     
+        }
+
+      }
+    }
+
+    return arr;
+
+}
+
+function isObject(obj){
+    return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+function isArray(obj){
+    return Object.prototype.toString.call(obj) === '[object Array]';
 }
 
 function formatXml(xml){
